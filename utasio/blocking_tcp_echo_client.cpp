f6907a8e3bc45c5cd5e7897c0496aa8616a01e9f
lialvin <li_alvin@qq.com>
2018-11-19 20:50:53 +0800

    1

--- utasio/blocking_tcp_echo_client.cpp
+++ utasio/blocking_tcp_echo_client.cpp
@@ -12,12 +12,15 @@
 #include <cstring>
 #include <iostream>
 #include <boost/asio.hpp>
+#include <thread> 
+#include<chrono>  
 
 using boost::asio::ip::tcp;
 
-enum { max_length = 1024 };
+enum { max_length = 10240 };
 
 void call(int argc , char * argv[]);
+void  printflow( std::time_t &  old_value, size_t reply_length, size_t & total_len );
 
 int main(int argc, char* argv[])
 {
@@ -29,9 +32,40 @@ int main(int argc, char* argv[])
     call(argc,argv);
 }
 
+/*
+*/
+void sndfun(tcp::socket   & s) //拷贝构造函数  
+{  
+    std::cout << "Thread " <<  " executing\n";  
+    //this_thread::sleep_for(chrono::milliseconds(10));  
+
+    char request[1028]={"strlen"};
+    for(int i=0;i<100;i++)
+    {
+       strcat(request,"higklmnopq");
+    }
+    
+    size_t request_length = std::strlen(request);
+
+    while(1)
+    {
+       boost::asio::write(s, boost::asio::buffer(request, request_length));
+       std::this_thread::sleep_for(std::chrono::milliseconds(10));  
+    }    
+    std::cout << "Thread finished " <<  " executing\n";  
+}
+
 void call(int argc , char * argv[])
 {
     boost::asio::io_context io_context;
+   
+    char request[1028]={"strlen"};
+    for(int i=0;i<100;i++)
+    {
+       strcat(request,"higklmnopq"); 
+    }
+
+    char reply[max_length];
 
     while(1)
     {
@@ -40,20 +74,50 @@ void call(int argc , char * argv[])
       boost::asio::connect(s, resolver.resolve(argv[1], argv[2]));
 
       // std::cout << "Enter message: ";
-      char request[max_length]={"send msg"};
+      //  char request[1025max_length]={"send msg"};
       // std::cin.getline(request, max_length);
-      size_t request_length = std::strlen(request);
-      boost::asio::write(s, boost::asio::buffer(request, request_length));
 
-      char reply[max_length];
-      size_t reply_length = boost::asio::read(s,
-      boost::asio::buffer(reply, request_length));
-      std::cout << "Reply is: ";
-      std::cout.write(reply, reply_length);
-      std::cout << "\n";
+       std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::duration<int, std::ratio<1>>> now_secondlevel = std::chrono::time_point_cast<std::chrono::duration<int, std::ratio<1>>>(std::chrono::high_resolution_clock::now());
+      std::time_t  ut_second = std::chrono::high_resolution_clock::to_time_t(now_secondlevel);      
+
+     size_t reply_length =0;
+     size_t total_len=0;
+
+     std::thread t3(sndfun, std::ref(s)); //引用  
+     while(1) 
+     {
+      //   size_t request_length = std::strlen(request);
+      //   boost::asio::write(s, boost::asio::buffer(request, request_length));
+
+         reply_length = boost::asio::read(s,
+                             boost::asio::buffer(reply, max_length));
+
+         //std::cout<<"Reply length = "<< reply_length<<std::endl;
+
+         printflow(ut_second, reply_length, total_len );
+      } 
+
       boost::system::error_code ignored_ec;
       s.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ignored_ec);
-      sleep(600);
+      sleep(1);
     }
-
 }
+
+void  printflow( std::time_t &  old_value, size_t reply_length, size_t & total_len )
+{
+       std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::duration<int, std::ratio<1>>> now_secondlevel = std::chrono::time_point_cast<std::chrono::duration<int, std::ratio<1>>>(std::chrono::high_resolution_clock::now());
+      std::time_t  ut_second = std::chrono::high_resolution_clock::to_time_t(now_secondlevel);
+      // ut_def = std::chrono::high_resolution_clock::to_time_t(now_deflevel);
+
+     total_len+=reply_length;
+     if(ut_second > old_value+5) 
+     {
+         std::cout << ctime(&ut_second);
+         //std::cout << now_secondlevel << std::endl;
+         std::cout << ut_second << std::endl;
+         std::cout<<"Reply length total= "<< total_len<<"\n"<<std::endl;
+         old_value= ut_second;  
+         total_len=0;
+     }
+}
+
